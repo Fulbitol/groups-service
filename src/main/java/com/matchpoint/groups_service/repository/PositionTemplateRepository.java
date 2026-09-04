b49df16280ab5fe23c44d672513030314406d6e9
Felipe Munevar N <felipemunevarn@gmail.com>
2026-09-04 20:24:50 -0300

    feat: implement GroupService.createGroup with auto-generated PositionSlots

--- src/main/java/com/matchpoint/groups_service/repository/PositionTemplateRepository.java
+++ src/main/java/com/matchpoint/groups_service/repository/PositionTemplateRepository.java
@@ -3,5 +3,8 @@ package com.matchpoint.groups_service.repository;
 import com.matchpoint.groups_service.domain.PositionTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PositionTemplateRepository extends JpaRepository<PositionTemplate, Long> {
+    List<PositionTemplate> findByCategoryId(Long categoryId);
 }
